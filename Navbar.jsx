f892065d0e41be63d8388e92b985b9adf2272105
Tanu-Priya-Dev <tanupriya1608@gmail.com>
2024-09-19 17:54:15 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-pink-600 p-4 flex justify-between items-center relative">
+        <div className="text-white font-bold text-xl">
+         
+         
+        </div>
+
+        <div className="text-white text-lg font-semibold absolute left-1/2 transform -translate-x-1/2">
+          CURRENCY CONVERTER
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
